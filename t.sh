4422f304d1683c5974553975d6bb49788538857f
lcchua <laichwang@gmail.com>
2024-10-30 12:18:10 +0800

    debug and correct

--- t.sh
+++ t.sh
@@ -2,7 +2,7 @@
 
 list=$(git diff --name-only -r HEAD^1 HEAD)
 for file in ${list[@]}; do
-	if [[ "$file" != *'/test_paths.yml' ]]; then
+	if [[ "$file" != *"test_paths.yml" ]]; then
 		echo "$file"
 	fi
 done
